c8dc98eeb774816cb2dc3954508dbdb1d0264f75
Dallas Jones <drj2115@columbia.edu>
2021-10-12 17:45:40 -0400

    [MODULE-6] added starter app script

---
+++ assignment6/app.js
@@ -0,0 +1,11 @@
+(function() { // IIFE
+
+'use strict';
+
+angular.module('lunchCheckerApp', [])
+       .controller('lunchCheckerController', lunchCheckerController);
+
+function lunchCheckerController() {
+}
+
+})();
