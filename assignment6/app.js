b22f237528a2872dcc268cdf34b23203d695742b
Dallas Jones <drj2115@columbia.edu>
2021-10-12 18:14:32 -0400

    [MODULE-6] added functionality to count dishes on button click

--- assignment6/app.js
+++ assignment6/app.js
@@ -1,11 +1,15 @@
-(function() { // IIFE
-
+(function() {
 'use strict';
 
 angular.module('lunchCheckerApp', [])
        .controller('lunchCheckerController', lunchCheckerController);
 
-function lunchCheckerController() {
+function lunchCheckerController($scope) {
+    $scope.dishList = '';
+    $scope.countDishes = function() {
+        var numDishes = $scope.dishList.split(',').length;
+        $scope.lunchCheckerMessage = numDishes > 3 ? 'Too much!' : ($scope.dishList.trim().length < 1 ? '' : 'Enjoy!');
+    }
 }
 
 })();
