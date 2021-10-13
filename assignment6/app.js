765b52540908ba5d5889fc614119a3009eb84272
Dallas Jones <drj2115@columbia.edu>
2021-10-12 20:06:58 -0400

    [MODULE-6] updated names per requirements. Protect '$scope' from minification

--- assignment6/app.js
+++ assignment6/app.js
@@ -1,14 +1,14 @@
 (function() {
 'use strict';
 
-angular.module('lunchCheckerApp', [])
-       .controller('lunchCheckerController', lunchCheckerController);
-
-function lunchCheckerController($scope) {
+angular.module('LunchCheck', [])
+       .controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
     $scope.dishList = '';
     $scope.countDishes = function() {
         var numDishes = $scope.dishList.split(',').length;
-        $scope.lunchCheckerMessage = numDishes > 3 ? 'Too much!' : ($scope.dishList.trim().length < 1 ? '' : 'Enjoy!');
+        $scope.lunchCheckMessage = numDishes > 3 ? 'Too much!' : ($scope.dishList.length < 1 ? 'Please enter data first' : 'Enjoy!');
     }
 }
 
