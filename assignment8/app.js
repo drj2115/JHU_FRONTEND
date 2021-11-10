3790880b6038dea4109dc519f135d4ea5d6158c1
dallas <drj2115@columbia.edu>
2021-11-09 20:52:05 -0500

    [module8] added angular.min.js and app.js

---
+++ assignment8/app.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController ', NarrowItDownController )
+        .service('MenuSearchService', MenuSearchService);
+    NarrowItDownController .$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+    }
+
+    function MenuSearchService() {
+
+    }
+})();
