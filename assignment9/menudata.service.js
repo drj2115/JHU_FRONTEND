ee5b0cd760edf81a58fca2c604da6d1cacc5da6b
dallas <drj2115@columbia.edu>
2021-11-30 20:10:45 -0500

    [module9] added categories and items components

--- assignment9/menudata.service.js
+++ assignment9/menudata.service.js
@@ -1,14 +1,23 @@
 (function() {
     'use strict';
     angular.module('data', [])
-           .service('MenuDataService', MenuDataService);
+           .service('MenuDataService', MenuDataService)
+           .constant('restaurantUrl', 'https://davids-restaurant.herokuapp.com/')
+           .constant('categoriesUrl', restaurantUrl + 'categories.json')
+           .constant('itemsInCategoryUrl', restaurantUrl + 'menu_items.json');
 
     function MenuDataService() {
         this.getAllCategories = () => {
-
+            return $http({ method: "GET", url: categoriesUrl })
+                .then(response => {
+                    console.log('categories => ', response);
+                });
         };
-        this.getItemsForCategory = () => {
-
+        this.getItemsForCategory = (categoryShortName) => {
+            return $http({ method: "GET", url: itemsInCategoryUrl + '?category=' + categoryShortName })
+                .then(response => {
+                    console.log('items in ' + categoryShortName + ' => ', response);
+                });
         };
 
     }
